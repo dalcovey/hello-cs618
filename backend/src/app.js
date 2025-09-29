f1739aae50c28ea164f7fd294c7ac4fad3ae9fa0
Dallin Covey <covey.dallin@gmail.com>
2025-09-29 01:38:39 +0000

    chore: post, patch, delete routes added to posts.js. integrated bosyparser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,17 @@
 import express from 'express'
+import { postsRoutes } from './routes/posts.js'
+import bodyParser from 'body-parser'
+
 const app = express()
+app.use(bodyParser.json())
+postsRoutes(app)
+
 app.get('/', (req, res) => {
   res.send('Hello from Express Nodemon!')
 })
+
+app.get('/posts', (req, res) => {
+  res.send('These are your posts!')
+})
+
 export { app }
