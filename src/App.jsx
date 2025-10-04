f6a2e9112e9a13076685be5ac586caba5141e264
Dallin Covey <covey.dallin@gmail.com>
2025-10-04 19:48:15 +0000

    chore: create frontend components PostList.jsx and Post.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel="noreferrer">
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel="noreferrer">
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Hello CS618</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p></p>
-      </div>
-    </>
-  )
+import { PostList } from './components/PostList.jsx'
+const posts = [
+  {
+    title: 'Full-Stack React Projects',
+    contents: "Let's become full-stack developers!",
+    author: 'Daniel Bugl',
+  },
+  { title: 'Hello React!' },
+]
+export function App() {
+  return <PostList posts={posts} />
 }
-
-export default App
